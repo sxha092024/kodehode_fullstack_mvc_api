c4de1929275c4b86c3f3a8461f17b115cfcd70a9
sxha092024 <183362499+sxha092024@users.noreply.github.com>
2025-01-29 10:31:53 +0100

    start sketching data model

---
+++ BloggoApi/Contexts/SqliteDbContext.cs
@@ -0,0 +1,56 @@
+using BloggoApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.ValueGeneration;
+
+namespace BloggoApi.Contexts;
+
+// UUIDv7 keys have the very nice property of when sorted, they are also ordered by creation time
+// Unfortunately, with the relative freshness of the standard, there are few DBs or frameworks which
+// natively support it in their generators / value providers
+internal class V7Generator : ValueGenerator<Guid>
+{
+    public override bool GeneratesTemporaryValues => false;
+
+    public override Guid Next(EntityEntry entry)
+    {
+        return Guid.CreateVersion7();
+    }
+}
+
+public class SqliteDbContext : DbContext
+{
+    public DbSet<BlogPost> BlogPosts { get; set; }
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=Database/Bloggo.db; foreign keys=true");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>().Property(u => u.UserId).HasValueGenerator(typeof(V7Generator));
+        // exceedingly annoying but this is the way in which efcore defines a unique constraint on the table side
+        modelBuilder.Entity<User>().HasAlternateKey(u => u.UserName);
+        modelBuilder
+            .Entity<User>()
+            .HasMany(u => u.OwnedPosts)
+            .WithOne(b => b.Owner)
+            .HasForeignKey(b => b.OwnerId)
+            .IsRequired();
+        modelBuilder.Entity<User>().HasMany(u => u.AuthoredPosts).WithMany(b => b.Authors);
+
+        modelBuilder
+            .Entity<BlogPost>()
+            .Property(b => b.OwnerId)
+            .HasValueGenerator(typeof(V7Generator));
+        modelBuilder
+            .Entity<BlogPost>()
+            .HasOne(b => b.Owner)
+            .WithMany(u => u.OwnedPosts)
+            .HasForeignKey(b => b.OwnerId)
+            .IsRequired();
+        modelBuilder.Entity<BlogPost>().HasMany(b => b.Authors).WithMany(u => u.AuthoredPosts);
+    }
+}
---
+++ BloggoApi/Models/BlogPost.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BloggoApi.Models;
+
+public class BlogPost
+{
+    [MaxLength(36)]
+    public Guid BlogPostId { get; set; }
+
+    [MaxLength(36)]
+    public Guid OwnerId { get; set; }
+    public ICollection<User> Authors { get; set; } = [];
+    public User Owner { get; set; } = null!;
+    public required string Title { get; set; } = string.Empty;
+    public required string Content { get; set; } = string.Empty;
+    public required bool published { get; set; }
+}
