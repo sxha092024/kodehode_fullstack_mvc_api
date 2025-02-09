afdae50b01a03c4f31dc5c04a663fb9e1e3cdd66
sxha092024 <183362499+sxha092024@users.noreply.github.com>
2025-02-09 23:21:00 +0100

    side-step service lifetime issues
    Note this implementation currently blocks the main thread, needs to wrap
    the underlying task such that the instanciating thread isn't blocked

--- BloggoApi/Services/DeletionService.cs
+++ BloggoApi/Services/DeletionService.cs
@@ -1,64 +1,71 @@
 using BloggoApi.Contexts;
 using BloggoApi.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BloggoApi.Services;
 
-public class DeletionService(ILogger<DeletionService> logger, SqliteDbContext context)
+public class DeletionService(ILogger<DeletionService> logger, IServiceScopeFactory serviceScope)
     : BackgroundService
 {
     private readonly ILogger<DeletionService> _logger = logger;
-    private readonly SqliteDbContext db = context;
+    private readonly IServiceScopeFactory _scopeFactory = serviceScope;
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        _logger.LogInformation("DeletionService is starting");
-        stoppingToken.Register(() =>
+        // TODO: wrap this to stop blocking the main thread from further execution
+        using (var scope = _scopeFactory.CreateScope())
         {
-            _logger.LogInformation("DeletionService background task is stopping.");
-        });
-
-        while (!stoppingToken.IsCancellationRequested)
-        {
-            var overdue = db.ScheduledDeletions.Where(d => d._when <= DateTimeOffset.UtcNow);
-
-            foreach (var item in overdue)
+            var db = scope.ServiceProvider.GetRequiredService<SqliteDbContext>();
+            _logger.LogInformation("DeletionService is starting");
+            stoppingToken.Register(() =>
             {
-                if (item.User is User user)
+                _logger.LogInformation("DeletionService background task is stopping.");
+            });
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var overdue = (await db.ScheduledDeletions.ToListAsync()).Where(d =>
+                    d._when <= DateTimeOffset.UtcNow
+                );
+
+                foreach (var item in overdue)
                 {
-                    _logger.LogCritical("deleting records associted with user {}", user.UserId);
-                    db.Remove(user);
-                    foreach (var authoredPost in user.AuthoredPosts)
+                    if (item.User is User user)
                     {
-                        _logger.LogCritical(
-                            "deleting blogpost with id {}",
-                            authoredPost.BlogPostId
-                        );
-                        db.Remove(authoredPost);
+                        _logger.LogCritical("deleting records associted with user {}", user.UserId);
+                        db.Remove(user);
+                        foreach (var authoredPost in user.AuthoredPosts)
+                        {
+                            _logger.LogCritical(
+                                "deleting blogpost with id {}",
+                                authoredPost.BlogPostId
+                            );
+                            db.Remove(authoredPost);
+                        }
                     }
-                }
-                if (item.Post is BlogPost post)
-                {
-                    foreach (var author in post.Authors)
+                    if (item.Post is BlogPost post)
                     {
-                        _logger.LogCritical(
-                            "removing post {} from author {}",
-                            post.BlogPostId,
-                            author.UserId
-                        );
-                        author.AuthoredPosts.Remove(post);
+                        foreach (var author in post.Authors)
+                        {
+                            _logger.LogCritical(
+                                "removing post {} from author {}",
+                                post.BlogPostId,
+                                author.UserId
+                            );
+                            author.AuthoredPosts.Remove(post);
+                        }
+                        ;
+                        _logger.LogCritical("deleting blog post {}", post.BlogPostId);
+                        db.Remove(post);
                     }
-                    ;
-                    _logger.LogCritical("deleting blog post {}", post.BlogPostId);
-                    db.Remove(post);
+                    // ensure we don't re-fire events by keeping old schedules around
+                    db.Remove(item);
                 }
-                // ensure we don't re-fire events by keeping old schedules around
-                db.Remove(item);
+                // expeditiously propogate deletions by attempting to commit the transaction now
+                await db.SaveChangesAsync();
+                // TODO: turn into injected variable to allow tuning
+                Thread.Sleep(1000);
             }
-            // expeditiously propogate deletions by attempting to commit the transaction now
-            await db.SaveChangesAsync();
-            // TODO: turn into injected variable to allow tuning
-            Thread.Sleep(1000 * 60 * 10);
         }
-        _logger.LogInformation("cancelling {} background thread", this);
     }
 }
